cedaf79e857f940d812dd09c6afada597e5ebefd
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2025-01-24 10:07:38 +0100

    Update main.py

--- interlink/src/interlink/main.py
+++ interlink/src/interlink/main.py
@@ -1,17 +1,25 @@
 from dagger import dag, function, object_type
-from dagger.client.gen import Container, File
+from dagger.client.gen import Container, File, Service
 
 
 @object_type
 class Interlink:
+    name: str
+
     @function
-    async def interlink_cluster(self, values: File) -> Container:
-        k3s = dag.k3_s("test")
+    async def cluster_config(self, local: bool = False) -> File:
+        k3s = dag.k3_s(self.name)
+
+        k3s.config(local)
+
+    @function
+    async def interlink_cluster(self, values: File) -> Service:
+        k3s = dag.k3_s(self.name)
         server = k3s.server()
 
-        await server.start()
+        svc = await server.start()
 
-        return (
+        (
             dag.container()
             .from_("alpine/helm")
             .with_exec(["apk", "add", "kubectl"])
@@ -22,7 +30,6 @@ class Interlink:
                 [
                     "helm",
                     "install",
-                    "--wait",
                     "--debug",
                     "my-node",
                     "oci://ghcr.io/intertwin-eu/interlink-helm-chart/interlink",
@@ -31,3 +38,5 @@ class Interlink:
                 ]
             )
         )
+
+        return svc
